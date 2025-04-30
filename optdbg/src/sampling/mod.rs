f59b76f81a336ccf1976822feb3881302366c6e6
Yu Liu <yul5@cs.cmu.edu>
2025-04-30 18:52:53 -0400

    hash trees for eq check, compare plan property

--- optdbg/src/sampling/mod.rs
+++ optdbg/src/sampling/mod.rs
@@ -395,7 +395,7 @@ impl OptdOldBackend {
     ) -> Result<Vec<Plan>> {
         let opt = self.opt.as_mut().unwrap();
         let pl = self.plan.clone().unwrap();
-        let mut out = Vec::new();
+        let mut out = HashSet::new();
 
         let defaults: Vec<Arc<dyn Rule<DfNodeType, OptdCascadesOptimizer<DfNodeType>>>> = vec![
             Arc::new(rules::FilterInnerJoinTransposeRule::new()),
@@ -481,7 +481,7 @@ impl OptdOldBackend {
 
             if !out.contains(&phys_plan) && *self.best.as_ref().unwrap() != phys_plan {
                 println!("{}", phys_plan);
-                out.push(phys_plan);
+                out.insert(phys_plan);
                 if let RuleBailStrategy::Threshold(thres) = bail {
                     if out.len() == thres {
                         break;
@@ -490,7 +490,8 @@ impl OptdOldBackend {
                 println!("Have {} alternate plans", out.len());
             }
         }
-        Ok(out)
+
+        Ok(out.iter().cloned().collect_vec())
     }
 }
 
